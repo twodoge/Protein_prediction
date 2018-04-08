9b010342affaf48e977e01808f87546d5fbe1b3b
twodoge <twodoge@qq.com>
2018-04-08 19:44:25 +0800

    sample_1,拟合y_data的函数，权重和偏置分别趋近0.1和0.3

--- python grammer.py
+++ python grammer.py
@@ -38,5 +38,41 @@ def tensorflow_grammer():
             sess.run(update)
             print(sess.run(state))
 
+def sample_1():# 例子1，拟合y_data的函数，权重和偏置分别趋近0.1和0.3
+    #np.random.rand(100) 生成100个0-1之间的随机数，构成一维数组
+    #mp.random.rand(2,3) 生成2行3列的二维数组
+    x_data = np.random.rand(100).astype(np.float32)
+    y_data = x_data * 0.1 + 0.3
+
+    '''权重偏置这些不断更新的值用tf变量存储，
+    tf.random_uniform()的参数意义为：（shape,min,max）
+    偏置初始化为0
+    '''
+    weights = tf.Variable(tf.random_uniform([1],-1.0,1.0))
+    biases = tf.Variable(tf.zeros([1]))
+    # print(weights)
+    # print(biases)
+
+    y = weights * x_data +biases
+
+    #损失函数 tf.reduce_mean()是取平均值
+    loss = tf.reduce_mean(tf.square(y - y_data))
+
+    #用梯度优化方法最小化损失函数 learning_rate
+    optimizer = tf.train.GradientDescentOptimizer(0.5)
+    train = optimizer.minimize(loss)
+
+    #tf变量是需要初始化的，而且后边计算时还需要sess.run()一下
+    init = tf.global_variables_initializer()
+
+    #Session进行计算
+    with tf.Session() as sess:
+        sess.run(init)
+        for step in range(200):
+            sess.run(train)
+            if step in range(200):
+                if step%20 == 0:
+                    print (step, sess.run(weights), sess.run(biases))
+
 if __name__ == "__main__":#主函数
-    tensorflow_grammer()
+    sample_1()
